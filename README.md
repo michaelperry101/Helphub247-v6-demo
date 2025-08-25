f9d7a79cbab0599ee469c2e57b14f7eab4fba063
michaelperry101 <michael.perry94@outlook.com>
2025-08-25 20:39:22 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# HelpHub247 v6
+
+This package contains the v6 demo build.
---
+++ tailwind.config.js
@@ -0,0 +1,1 @@
+module.exports = { content: ['./app/**/*.{js,jsx,ts,tsx}','./components/**/*.{js,jsx,ts,tsx}','./pages/**/*.{js,jsx,ts,tsx}'], darkMode:'class', theme:{extend:{}}, plugins:[], };
